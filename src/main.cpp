3190e1e745ceff8f018c27fd9706f2a9bd53146d
sciencelight <642761715@qq.com>
2023-08-26 11:57:31 +0800

    平均数为k的最长连续子数组

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,52 @@
 #include "common.h"
-int func(unsigned int x, int y)
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int count(vector<int> &arr, int n, long long k)
 {
-    return ((x + y > 0) ? x * func(x, y + 1) : 3);
+    unordered_map<long long, long long> prefixSum; // 前缀和加上k * l1及其出现的位置
+    long long sum = 0;                             // 当前位置之前的元素和
+    long long maxLength = 0;                       // 最长子数组长度
+    for (long long i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        if (sum == k * (i + 1))
+        {
+            maxLength = i + 1;
+        }
+        else if (prefixSum.find(sum - k * (i + 1)) != prefixSum.end())
+        {
+
+            maxLength = max(maxLength, i - prefixSum[sum - k * (i + 1)]);
+        }
+
+        if (prefixSum.find(sum - k * (i + 1)) == prefixSum.end())
+        {
+            prefixSum[sum - k * (i + 1)] = i;
+        }
+    }
+    return maxLength > 0 ? maxLength : -1;
 }
-int main(int argc, char *argv[])
+int main()
 {
-    unsigned int x = 5;
-    int y = -7;
-    int c = 0;
-    c = func(x, y);
-    printf("c = %d\n", c);
-    system("pause");
+    int n, k, sum;
+    scanf("%d %d", &n, &k);
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("%d\n", count(arr, n, k));
     return 0;
 }
+/*
+5:
+5 3
+1 1 3 5 5
+
+10 6
+6 2 5 1 8 4 2 10 1 5
+*/
