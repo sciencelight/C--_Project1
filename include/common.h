c16c75c6be9e860cb8314dea61e625c57df70ad3
sciencelight <642761715@qq.com>
2023-12-10 23:29:30 +0800

    华为技术二面题目，二叉树的最值以及最长路径

--- include/common.h
+++ include/common.h
@@ -4,3 +4,56 @@
 #include <algorithm>
 #include <stdio.h>
 using namespace std;
+vector<vector<int>> direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+void split(vector<int> &nums, string line)
+{
+    while (line.find(',') != string::npos)
+    {
+        int pos = line.find(',');
+        nums.push_back(stoi(line.substr(0, pos)));
+        line = line.substr(pos + 1);
+    }
+    nums.push_back(stoi(line));
+}
+void getPmt(string &needle, vector<int> &pmt)
+{
+    int j = 0;
+    pmt[0] = 0;
+    int n = needle.length();
+    for (int i = 1; i < n; i++)
+    {
+        if (needle[i] == needle[j])
+        {
+            pmt[i] = j + 1;
+            j++;
+        }
+        else if (j == 0)
+            pmt[i] = 0;
+        else
+        {
+            j = pmt[j - 1];
+            i--;
+        }
+    }
+}
+int strStr(string haystack, string needle)
+{
+    int n1 = haystack.length(), n2 = needle.length();
+    if (n1 < n2)
+        return -1;
+    vector<int> pmt(n2);
+    getPmt(needle, pmt);
+    for (int i = 0, j = 0; i < n1; i++)
+    {
+        if (haystack[i] == needle[j])
+            j++;
+        else if (j > 0)
+        {
+            j = pmt[j - 1];
+            i--;
+        }
+        if (j == n2)
+            return i - j + 1;
+    }
+    return -1;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,102 +1,77 @@
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <stdio.h>
+#include <algorithm>
 #include <utility>
+#include <vector>
 using namespace std;
-vector<vector<int>> direction = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-vector<string> split_str(string st)
+struct node
 {
-    vector<string> ans;
-    while (st.find(" ") != string::npos)
+    int data;
+    node *left, *right;
+    node(int val)
     {
-        int pos = st.find(" ");
-        ans.push_back(st.substr(0, pos));
-        st = st.substr(pos + 1);
+        data = val;
+        left = nullptr;
+        right = nullptr;
     }
-    ans.push_back(st);
-    return ans;
+};
+void add_node(node *root, vector<int> &arr)
+{
+    arr.push_back(root->data);
+    if (root->left)
+        add_node(root->left, arr);
+    if (root->right)
+        add_node(root->right, arr);
+}
+int kmax(node *root, int k)
+{
+    if (!root)
+        return -1;
+    vector<int> arr;
+    add_node(root, arr);
+    sort(arr.begin(), arr.end(), greater<int>());
+    return arr[k - 1];
+}
+int mlen(node *root)
+{
+    if (root == nullptr)
+        return 0;
+    int mleft = mlen(root->left);
+    int mright = mlen(root->right);
+    return root->data + max(mleft, mright);
 }
 int main()
 {
-    // 矩阵读取
-    vector<vector<string>> matrix;
-    string line;
-    while (getline(cin, line))
-    {
-        if (line.size() == 0)
-            break;
-        else
-            matrix.push_back(split_str(line));
-    }
-    int n = matrix.size(), m = matrix[0].size();
-    // 数值转化
-    vector<vector<bool>> range(n, vector<bool>(m, false));
-    queue<pair<int, int>> que;
-    int needs = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            string st = matrix[i][j];
-            if (st == "NO")
-            {
-                range[i][j] = true;
-                needs++;
-            }
-            else if (st == "YES")
-                que.push({i, j});
-        }
-    }
-    // 特判
-    if (needs == 0 || range.size() == 0)
-    {
-        printf("0\n");
-        return 0;
-    }
-    // 广度优先搜索
-    int turns = 0, cirles = 0;
-    while (!que.empty())
-    {
-        cirles++;
-        int size = que.size();
-        for (int i = 0; i < size; i++)
-        {
-            pair<int, int> node = que.front();
-            que.pop();
-            for (int k = 0; k < 4; k++)
-            {
-                int x = node.first + direction[k][0], y = node.second + direction[k][1];
-                if (x < 0 || y < 0 || x >= n || y >= m)
-                    continue;
-                if (range[x][y])
-                {
-                    turns++;
-                    que.push({x, y});
-                    range[x][y] = false;
-                }
-            }
-        }
-    }
-    if (needs == turns)
-        printf("%d\n", cirles - 1);
-    else
-        printf("-1\n");
+    node *head = new node(4);
+    node *n1, *n2, *n3, *n4;
+    n1 = new node(2);
+    n2 = new node(1);
+    n3 = new node(3);
+    n4 = new node(5);
+    head->left = n1;
+    head->right = n2;
+    n1->left = n3;
+    n1->right = n4;
+    printf("%d\n%d\n", kmax(head, 3), mlen(head));
     return 0;
 }
 /*
-测试数据：
-YES YES NO
-NO NO NO
-YES NO NO
+7,0 7,1 6,1 5,0 5,2 4,4
+7,0 6,1 7,1 5,0 5,2 4,4
+5,0 7,0 6,1 7,1 5,2 4,4
+5,0 7,0 5,2 6,1 7,1 4,4
+5,0 7,0 5,2 6,1 4,4 7,1
 
-YES NO NO NO
-NO NO NO NO
-NO NO NO NO
-NO NO NO NO
 
-YES NO NO YES
-NO NO YES NO
-NO YES NA NA
-YES NO NA NO
 
-*/
+
+
+
+
+
+4,4 5,0 5,2 6,1 7,0 7,1
+5,0 5,2 6,1 7,0 4,4 7,1
+5,0 6,1 5,2 7,0 4,4 7,1
+5,0 5,2 7,0 4,4 6,1 7,1
+
+ */
